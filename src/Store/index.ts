39a7493d7af83aac9aa01650939e6c0023583613
sreenivasa.devarinti@xcubelabs.com <sreenivasa.devarinti@xcubelabs.com>
2022-02-07 17:01:23 +0530

    {Task} persistor added to redux state

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -1,14 +1,14 @@
 import rootReducer from "../Reducer/index"
 import {createStore} from 'redux'
-// import { persistReducer, persistStore } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-// const persistConfig= {key:'root',storage}
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistConfig= {key:'root',storage}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 
-export const store=createStore(rootReducer);
+export const store=createStore(persistedReducer);
 
-// export const persistor = persistStore(store)
+export const persistor = persistStore(store)
 
 export  default store;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,15 +2,17 @@ import { Link } from "react-router-dom";
 import {useRef} from 'react'
 import store from "../Store";
 import app_Actions from '../Actions/Actions'
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function Login() {
   const UserName = useRef<HTMLInputElement>(null)
   const UserPassword= useRef<HTMLInputElement>(null)
+  const dispatch = useDispatch();
   const setValues = () =>{
     if(UserName.current?.value && UserPassword.current?.value){
       console.log(UserName.current.value)
-      store.dispatch(app_Actions.setResgistUser({UserName :UserName.current?.value , UserPassword:UserPassword.current?.value}))
+      dispatch(app_Actions.setResgistUser({UserName :UserName.current?.value , UserPassword:UserPassword.current?.value}))
+      // store.dispatch(app_Actions.setResgistUser({UserName :UserName.current?.value , UserPassword:UserPassword.current?.value}))
     }
   }
   const result: any = useSelector((state: any) => state);
