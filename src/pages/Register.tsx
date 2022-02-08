de5400d341d5a524d9dfe6dbe4696e7f72e149b6
Roja <chinthalapudiroja21@gmail.com>
2022-02-08 17:22:05 +0530

    Merge Conflicts Resolved

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,40 +1,66 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useRef } from 'react';
+import { useRef } from "react";
+import { Card, CardContent } from "@material-ui/core";
 
 function Register() {
-  const Name = useRef({value:'',error:false})
-  const password = useRef({value:'',error:false})
-  const email = useRef({value:'',error:false})
-  const number = useRef({value:'',error:false})
-  const confirmPassword = useRef({value:'',error:false})
+  const Name = useRef({ value: "", error: false });
+  const password = useRef({ value: "", error: false });
+  const email = useRef({ value: "", error: false });
+  const number = useRef({ value: "", error: false });
+  const confirmPassword = useRef({ value: "", error: false });
 
-  let navigate = useNavigate(); 
-  const registerData=[Name,password,email,number,confirmPassword]
+  let navigate = useNavigate();
+  const registerData = [Name, password, email, number, confirmPassword];
 
-  const gotoSideBar=()=>{
+  const gotoSideBar = () => {
     console.log("goto home page");
-    // let path = `/home`; 
+    // let path = `/home`;
     // navigate(path);
-  }
-  const goToLogin=()=>{
-    let path = `/`; 
+  };
+  const goToLogin = () => {
+    let path = `/`;
     navigate(path);
-  }
+  };
   return (
-    <div className="register">
-      <div className="registration-form">
-      <input type="text" placeholder="enter name"></input> 
-      <input type="email" placeholder="enter email"></input> 
-      <input type="number" placeholder="number" maxLength={10}></input> 
-      <input type="password" placeholder="set password"></input>
-      <input type="password" placeholder="confirm password"></input> 
-      </div>
-      <button onClick={()=>{gotoSideBar()}}>Register</button>
-      <div className="login_register">
-    Have an account! 
-    <div onClick={()=>{goToLogin()}} className="register_text">Login Here</div>
-    </div>
+    <div className="background">
+      <Card className="card">
+        <CardContent>
+          <div>
+            <h1>User Registration</h1>
+          </div>
+          <form>
+            {/* Labels and inputs for form data */}
+            <label className="label">Name</label>
+            <input className="input" type="text" />
+
+            <label className="label">Email</label>
+            <input className="input" type="email" />
+
+            <label className="label">Password</label>
+            <input className="input" type="password" />
+
+            <button className="btn"
+              onClick={() => {
+                gotoSideBar();
+              }}
+            >
+              Register
+            </button>
+            <div className="login_register">
+              Have an account!
+              <div
+                onClick={() => {
+                  goToLogin();
+                }}
+                className="register_text"
+              >
+                Login Here
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
