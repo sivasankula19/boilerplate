a96951c824303e4857c6ae2a498ea23539880b0d
nikitha singireddy <nikithareddy.singireddy@xcubelabs.com>
2022-02-08 14:33:48 +0530

    Added register page content

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import { Card, CardContent } from "@material-ui/core";
+import React from "react";
 
 function Register() {
   return (
-    <div className="register">
-        Register Page
+    <div className="background">
+      <Card className="card">
+        <CardContent>
+          <div>
+            <h1>User Registration</h1>
+          </div>
+          <form>
+            {/* Labels and inputs for form data */}
+            <label className="label">Name</label>
+            <input className="input" type="text" />
+
+            <label className="label">Email</label>
+            <input className="input" type="email" />
+
+            <label className="label">Password</label>
+            <input className="input" type="password" />
+
+            <button className="btn" type="submit">
+              Submit
+            </button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
