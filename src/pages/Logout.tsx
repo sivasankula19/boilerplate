d2812be7bd9b16814a8107f6cc616649e7659abc
Siva <sivaiah.sankula@xcubelabs.com>
2022-02-09 18:42:40 +0530

    {loader}Loader added

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { loaderService } from '../Services/LoaderService';
 
 function Logout() {
+
+  const loaderPage = () => {
+    setTimeout(() => {
+      loaderService.hide()
+    }, 3000);
+    console.log('show')
+    loaderService.show()
+  }
   return (
     <div className="register">
         Logout Page
+        <button onClick={loaderPage}>loader</button>
     </div>
   );
 }
