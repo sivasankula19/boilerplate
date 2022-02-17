9a62dbcc209b12dfce75b6ea4befdf634b5670a3
nikitha singireddy <nikithareddy.singireddy@xcubelabs.com>
2022-02-17 20:34:38 +0530

    Added webpack version 5

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    // list of extensions that has to be resolved automatically. With this options there is no need of adding file extension while importing
+    extensions: [
+      ".webpack.js",
+      ".web.js",
+      ".tsx",
+      ".ts",
+      ".js",
+      ".json",
+      ".scss",
+      ".css",
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|.tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        // Fonts laoder
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader?name=./fonts/[name].[hash:6].[ext]"],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              compilerOptions: { noEmit: false },
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        // Images loader
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader?name=./images/[name].[hash:6].[ext]"],
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[contenthash].[ext]",
+            outputhPath: "imgs",
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
